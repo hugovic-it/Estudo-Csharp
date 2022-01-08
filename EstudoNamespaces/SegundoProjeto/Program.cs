f10c70686ce75e468bab4cf2bc89d5bf75817cd2
hugovic-it <hugovic.it@gmail.com>
2022-01-08 10:27:58 -0300

    segundo projeto inserido

---
+++ EstudoNamespaces/SegundoProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SegundoProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olar segundo projeto");
+        }
+    }
+}
